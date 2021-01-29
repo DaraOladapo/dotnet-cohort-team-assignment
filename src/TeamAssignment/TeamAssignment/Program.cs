863ae00a79bd636917b074cfecc5eefd4ca18829
andrewklein98 <aklein@qa.com>
2021-01-29 09:57:01 +0000

    added in the names!

--- src/TeamAssignment/TeamAssignment/Program.cs
+++ src/TeamAssignment/TeamAssignment/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TeamAssignment
 {
     class Program
     {
+        public static List<string> People = new List<string> { "AndrewK", "AndrewO", "Winie", "Ben", "Mathew", "Adam", "Gabi", "Stephen", "Usama", "James", "Olga" };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach(var a in People)
+            {
+                Console.WriteLine(  a);
+            }
         }
     }
 }
